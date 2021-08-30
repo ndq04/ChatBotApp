9307cdf502d270b9dfe73df9319cbda9fdad7c36
Duy Quang <duyquang412000@gmail.com>
2021-08-30 22:32:42 +0700

    edit chatbot.py file

--- ChatBot_Python/chatbot.py
+++ ChatBot_Python/chatbot.py
@@ -1,31 +1,7 @@
 from chatterbot import ChatBot
-from flask import Flask, request
-from chatterbot.trainers import ChatterBotCorpusTrainer
-from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
-CORS(app, resources={r"/api/*":{"origins":"*"}})
-app.config["CORS HEADERS"] = "Content-Type"
-
-bot = ChatBot(
-  "ChatBot", 
-  storage_adapter="chatterbot.storage.SQLStorageAdapter",
+myBot = ChatBot(
+  "My ChatBot", 
   filters=["chatterbot.filters.RepetitiveResponseFilter"]
 )
-tranner = ChatterBotCorpusTrainer(bot)
-tranner.train("./knowledges.yml")
 
-@app.route("/")
-@cross_origin()
-def Home():
-  return str("Welcome Home")
-
-@app.route("/get", methods=['POST'])
-@cross_origin()
-def user():
-  user_input = request.json["message"]
-  response = bot.get_response(user_input)
-  return str(response)
-
-if __name__ == "__main__":
-  app.run(debug=True)
